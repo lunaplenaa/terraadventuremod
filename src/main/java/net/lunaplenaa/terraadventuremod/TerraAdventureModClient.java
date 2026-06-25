1f4abc47826f4e3facaae5d6e8893526f0093a56
yue <lunacarmina25@gmail.com>
2026-06-24 20:24:02 -0700

    file restructuring and whatnot, added assets

---
+++ src/main/java/net/lunaplenaa/terraadventuremod/TerraAdventureModClient.java
@@ -0,0 +1,26 @@
+package net.lunaplenaa.terraadventuremod;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = TerraAdventureMod.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = TerraAdventureMod.MODID, value = Dist.CLIENT)
+public class TerraAdventureModClient {
+    public TerraAdventureModClient(ModContainer container) {
+
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
